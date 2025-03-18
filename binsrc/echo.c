1e17cb1ec7ace997fbf56113635b0779b3eac18f
pravda <theusernameistakentrywiki@gmail.com>
2025-03-18 11:31:43 -0700

    made echo bin

--- binsrc/echo.c
+++ binsrc/echo.c
@@ -1,7 +1,13 @@
+#include <unistd.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
 
-	char buffer[] = argv[1];
-	char* buffer_size = argv[2];	
+	char* buffer = argv[1];
+	int buffer_size = atoi(argv[2]);	
 
 	write(STDOUT_FILENO, buffer, buffer_size);
+	write(STDOUT_FILENO, "\n", 1);
+
+	return 0;
 }
